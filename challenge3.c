a75fe64eec496efb0c0922f06211b09ca7202565
basma <bassmaeljazzouli@gmail.com>
2023-09-14 18:49:29 +0100

    challenge3

---
+++ challenge3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+ int b ;
+ int a ;
+
+printf("a : ");
+scanf("%d", &a);
+printf("b : ");
+scanf("%d", &b);
+
+
+printf("a+b= %d\n",a+b);
+printf("a*b= %d\n",a*b);
+printf("a/b= %d\n",a/b);
+printf("a%%b= %d\n",a%b);
+ 
+
+
+
+
+
+
+
+    return 0;
+}
